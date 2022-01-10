51f8b85eb571a863f555ada64c7234fc19cf09ca
Ruvick <rudikov.web@ya.ru>
2022-01-10 18:09:52 +0300

    Вывел наличие в карточках

--- template-parts/product-elem.php
+++ template-parts/product-elem.php
@@ -4,7 +4,16 @@
 			<img src="<?php  $imgTm = get_the_post_thumbnail_url( get_the_ID(), "tominiatyre" ); echo empty($imgTm)?get_bloginfo("template_url")."/img/no-photo.jpg":$imgTm; ?>" alt="<? the_title();?>"> 
 		</div>
 		<h6 class="card__title card-pr__title"><? the_title();?></h6>
-		<span class="card-pr__availability">В наличии, более 3 шт.</span>
+		<?php
+			$jachejka = carbon_get_the_post_meta('offer_nal');
+				if (strlen($jachejka) == 0) {
+					echo '<span class="card-pr__availability">Уточняйте наличие</span> ';
+				} else if ($jachejka === 0 || $jachejka === '0') {
+					echo '<span class="card-pr__availability">Нет в наличии</span> ';
+				} else {
+					echo '<span class="card-pr__availability">В наличии, более 3 шт.</span> ';
+				}
+		?>
 		<p class="card__price rub"><?echo carbon_get_post_meta(get_the_ID(),"offer_price"); ?> </p>
 		<? $sticker = carbon_get_post_meta(get_the_ID(),"offer_sticker");
     	if (!empty($sticker)) {?>
